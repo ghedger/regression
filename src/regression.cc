53222da8d99510699da6b928ceb20f637aef89d9
ghedger <greg@hedgersoftware.com>
2020-07-02 18:22:04 -0600

    Added better instructions; added ability to load in a CSV (or other-SV) file; added option to swap x and y values in loaded file (e.g. {y, x}, {y, x} instead of {x, y}, {x, y}).

--- src/regression.cc
+++ src/regression.cc
@@ -19,6 +19,11 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <vector>
+
+using namespace std;
 
 namespace hedger {
   struct DataPoint {
@@ -28,10 +33,17 @@ namespace hedger {
 
   void printUsage() {
     printf("regression\n");
+    printf("Ordinary Least Squares (OLS) linear regression analysis.\n");
     printf("Calculates Y baseline b and slope m from set of {x,y} points.\n");
     printf("Copyright (C) 2020 Greg Hedger\n");
-    printf("Usage:\n");
+    printf("\nOptions:\n");
+    printf("  -f Specify CSV or other non-digit-separated file\n");
+    printf("  -xf Specify file and swap x and y values\n");
+    printf("\nUsage:\n");
     printf(" regression [x₁] [y₁] ... [xₙ] [yₙ]\n");
+    printf(" regression -f [csv_file]\n");
+    printf(" regression -xf [csv_file]\n");
+    printf("CSV files can use any non-digit separator.");
   }
 
   // getSums
@@ -146,27 +158,103 @@ namespace hedger {
     *b = (size * sigmaXY) - (sigmaX * sigmaY);
     *b /= (size * sigmaXSquared) - (sigmaX * sigmaX);
   }
+
+  // parseFile
+  // Parses a CSV or other non-numeric value separated file and gets the data into an array of DataPoints.
+  // Entry: filename
+  // Exit: pointer to DataPoint array
+  DataPoint *parseFile(const char *file, int *data_size) {
+    vector<double> x, y;
+    int tuples = 0;
+    bool xy = false;
+    const int MAX_DIGITS = 256;
+    char accum[MAX_DIGITS], c;
+    int aidx = 0; // accumulator index
+    double d = 0.0;
+    DataPoint *data = NULL;
+    FILE *f = fopen(file, "r");
+    if (f) {
+      while ((c = fgetc(f)) != EOF) {
+        //printf("%c", c);
+        if (isdigit(c) || '.' == c || '-' == c) {
+          if (aidx > MAX_DIGITS) {
+            x.clear();
+            y.clear();
+            // Error;
+            return NULL;
+          }
+          accum[aidx++] = c; //  read into number accumulator
+        } else {
+          // Nondigit,nunumeric; terminate, read whole # from accumulator
+          accum[aidx] = '\0'; // null terminate
+          aidx = 0; // reset accum index
+          sscanf(accum, "%lf", &d);
+          if (xy) {
+            y.push_back(d);
+            tuples++;
+          } else {
+            x.push_back(d);
+          }
+          xy ^= true; // Toggle x/y
+        }
+      }
+      // Allocate data source and free up lists
+      data = new DataPoint[tuples];
+      for (auto i = 0; i < tuples; i++) {
+        data[i].x = x[i];
+        data[i].y = y[i];
+      }
+      x.clear();
+      y.clear();
+      // Set data size
+      *data_size = tuples;
+    }
+    return data;
+  }
+
 } // namespace hedger
 
 static const int DATA_SIZE = 6;
 int main(int argc, const char *argv[])
 {
   using namespace hedger;
+  DataPoint *data = NULL;
+  int data_size = 0;
 
   if (argc < 5) {
-    printUsage();
-    return 1;
+    if (argc > 2) {
+      if (!strcmp(argv[1], "-f") || !strcmp(argv[1], "-xf")) {
+        data = parseFile(argv[2], &data_size);
+        if (!data) {
+          printf("Could not read or allocate data, file '%s'\n", argv[2]);
+          return -1;
+        }
+        if (!strcmp(argv[1], "-xf")) {
+          for (auto i = 0; i < data_size; i++) {
+            double swap = data[i].y;
+            data[i].y = data[i].x;
+            data[i].x = swap;
+          }
+        }
+      }
+    } else {
+      printUsage();
+      return 1;
+    }
   }
 
-  // Pull in arguments from command line
-  int data_size = (argc - 1) / 2;
-  DataPoint *data = new DataPoint[ data_size ];
-  double v;
-  for (int i = 0; i < data_size; i++) {
-    sscanf(argv[ 1 + i * 2 ], "%lf", &v);
-    data[ i ].x = v;
-    sscanf(argv[ 1 + i * 2 + 1 ], "%lf", &v);
-    data[ i ].y = v;
+  // Pull in arguments from command line, if and only if we are not
+  // parsing from a file.
+  if (!data) {
+    data_size = (argc - 1) / 2;
+    data = new DataPoint[ data_size ];
+    double v;
+    for (int i = 0; i < data_size; i++) {
+      sscanf(argv[ 1 + i * 2 ], "%lf", &v);
+      data[ i ].x = v;
+      sscanf(argv[ 1 + i * 2 + 1 ], "%lf", &v);
+      data[ i ].y = v;
+    }
   }
 
   // Warn if odd # of arguments
@@ -177,7 +265,7 @@ int main(int argc, const char *argv[])
   // Get the Y baseline ("b") and slope ("m")
   double m = 0.0, b = 0.0;
   getBestFit( data, data_size, &b, &m );
-  printf("Best fit:\n");
+  printf("Best fit (OLS):\n");
   printf("b=%lf\nm=%lf\n", b, m);
 
   double xbar = getMean( data, data_size );
